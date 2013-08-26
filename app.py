0ce5ff18aa3e6a84c0caa2b2a80b51999b3ae6ac
Asaf Paris-Mandoki <asafpm@gmail.com>
2013-08-26 21:08:26 +0100

    Cleaned up code

--- app.py
+++ app.py
@@ -16,9 +16,6 @@ from struct import unpack
 
 import sys
 
-global lock
-lock = threading.Lock()
-
 
 class Widget():
     def __init__(self, rect):
@@ -28,35 +25,49 @@ class Widget():
         pass
         
 class Oscilloscope(Widget):
-    def __init__(self,rect, dr):
+    def __init__(self,rect, data_reader):
         Widget.__init__(self,rect)
-        self.dr = dr
-
+        data_reader.add_point_listener(self.point_added)
+        data_reader.add_turn_listener(self.turn_finished)
+        
+        self.datacx = np.zeros(data_reader.data_buff_size) #Current data
+        self.datacy = np.zeros(data_reader.data_buff_size)
+        
+        self.datapx = self.datapy = None #Previous data
+        self.i = 0
+        
+        
+    def point_added(self,x,y,i):
+        self.datacx[i] = x
+        self.datacy[i] = y
+        self.i = i
+        
+    def turn_finished(self,xs, ys):
+        self.datapx = xs
+        self.datapy = ys
       
     def draw(self, surface):
         surface.fill((255,255,255))
-        lock.acquire()
-        x = np.array(self.dr.data)[:self.dr.i]
-        y = np.array(self.dr.datay)[:self.dr.i]
-        if len(x>0):
-            xmax = x.max()
-            q = np.nonzero(self.dr.data2 > xmax)
-            x2 = self.dr.data2[q]
-            y2 = self.dr.datay2[q]
-        else:
-            x2 = np.array([])
-            y2 = np.array([])
-            
-        lock.release()
+        xc = self.datacx[:self.i]
+        yc = self.datacy[:self.i]
         self.draw_grid(surface)
-        self.plot(surface,x,y, 0,2*np.pi, 0,1024)
-        self.plot(surface,x2,y2, 0,2*np.pi, 0,1024)
+        
+        if self.datapx != None: #If there is any previous data to draw
+            if len(xc>0):
+                xmax = xc.max()
+                q = np.nonzero(self.datapx > xmax) #Find where should we display previous data
+                xp = self.datapx[q]
+                yp = self.datapy[q]
+            else:
+                xp = np.array([])
+                yp = np.array([])
+                
+            self.plot(surface,xp,yp, 0,2*np.pi, 0,1024) #Plot previous data
+        self.plot(surface,xc,yc, 0,2*np.pi, 0,1024) #Plot current data
         
     def draw_grid(self, surface):
         w, h = surface.get_size()
-        w -= 1
-        h -= 1
-        
+        w, h = w-1, h-1
         #Draw grid
         for i in range(11):
             pygame.draw.line(surface, (210, 210, 210), (0,int(h*0.1*i)), (w-1,int(h*0.1*i)), 1)
@@ -66,8 +77,7 @@ class Oscilloscope(Widget):
         
     def plot(self, surface, x, y, xmin, xmax, ymin, ymax):
         w, h = surface.get_size()
-        w -= 1
-        h -= 1
+        w, h = w-1, h-1
         
         #Scale data
         xspan = abs(xmax-xmin)
@@ -84,60 +94,67 @@ class Oscilloscope(Widget):
                                                         (int(xp[i+1]),int(yp[i+1])), 1)
         except IndexError:
             print "Error: ",i
-        
-    """
-    def plot(self, surface, xmin, xmax, ymin, ymax):
-        
-        w, h = surface.get_size()
-        lock.acquire()
-        x = np.array(self.dr.data)[:self.dr.i]
-        y = np.array(self.dr.datay)[:self.dr.i]
-        lock.release()
-        si = np.argsort(x)
-        x = x[si]
-        y = y[si]
-        
-
-        xspan = abs(xmax-xmin)
-        yspan = abs(ymax-ymin)
-        xsc = 0.9*w/xspan
-        ysc = 0.9*h/yspan
-        xp = (x-xmin+0.05)*xsc
-        yp = (y-ymin+0.05)*ysc
-        for i in range(len(xp)-2):
-            #pygame.draw.circle(surface, (0, 255, 0), (int(xp[i]),int(yp[i])), 1)
-            pygame.draw.line(surface, (0, 0, 255), (int(xp[i]),int(yp[i])), (int(xp[i+1]),int(yp[i+1])), 1)
-            
-        #pygame.draw.line(surface, (180, 180, 180), (0,int(h*0.5)), (w-1,int(h*0.5)), 1)
-    """
-        
-def stokes(th, i):
-
-    th = np.array(th)
-    i = np.array(i)
-    signal0 = i
     
-    signal1 = i*np.sin(2*th)
-    signal2 = i*np.cos(4*th)
-    signal3 = i*np.sin(4*th)
-   
+class StokesCalculator():
+    def __init__(self,data_reader, wireframe):
+        data_reader.add_turn_listener(self.turn_finished)
+        self.wireframe = wireframe
+        self.s0 = [0]*5
+        self.s1 = [0]*5
+        self.s2 = [0]*5
+        self.s3 = [0]*5
+        
+    def stokes(self,th, i):
 
-    a = np.trapz(signal0, th)*1/np.pi
-    b = np.trapz(signal1, th)*2/np.pi
-    c = np.trapz(signal2, th)*2/np.pi
-    d = np.trapz(signal3, th)*2/np.pi
+        th = np.array(th)
+        i = np.array(i)
+        signal0 = i
+        
+        signal1 = i*np.sin(2*th)
+        signal2 = i*np.cos(4*th)
+        signal3 = i*np.sin(4*th)
+       
 
-    s0 = a-c
-    s1 = 2*c
-    s2 = 2*d
-    s3 = b
-    
-    #phi = 0.5*np.arctan(s2/s1)
-    #xi = 0.5*np.arctan(s3/np.sqrt(s1**2+s2**2))
-    phi = 0.5*np.arctan2(s2,s1)
-    xi = 0.5*np.arccos(s3/np.sqrt(s1**2+s2**2+s3**2))
-    
-    return s0, s1, s2, s3
+        a = np.trapz(signal0, th)*1/np.pi
+        b = np.trapz(signal1, th)*2/np.pi
+        c = np.trapz(signal2, th)*2/np.pi
+        d = np.trapz(signal3, th)*2/np.pi
+
+        s0 = a-c
+        s1 = 2*c
+        s2 = 2*d
+        s3 = b
+
+        phi = 0.5*np.arctan2(s2,s1)
+        xi = 0.5*np.arccos(s3/np.sqrt(s1**2+s2**2+s3**2))
+        
+        return s0, s1, s2, s3
+        
+    def turn_finished(self,xs, ys):
+
+        size = len(ys)
+        
+        s0, s1, s2, s3 = self.stokes(np.linspace(0,2*np.pi,size),ys)
+        # Calculate 5 point moving average
+        """
+        self.s0.pop()
+        self.s1.pop()
+        self.s2.pop()
+        self.s3.pop()
+        self.s0.insert(0,s0)
+        self.s1.insert(0,s1)
+        self.s2.insert(0,s2)
+        self.s3.insert(0,s3)
+        s0 = sum(self.s0)/len(self.s0)
+        s1 = sum(self.s1)/len(self.s1)
+        s2 = sum(self.s2)/len(self.s2)
+        s3 = sum(self.s3)/len(self.s3)
+        """
+        (x,y,z), r = (0.5,0.5, 0.5), 0.4
+        if s0 > 0: #If the intensity is greater than zero
+            l = np.sqrt(s1**2+s2**2+s3**2)
+            self.wireframe.addNodes([(x + r*s1/l, y + r*s3/l, z + r*s2/l )])
+        self.wireframe.discardOldNodes(60)
         
 class WireframeDecorator():
     def __init__(self, wireframe, **kwargs):
@@ -161,7 +178,7 @@ class WireframeDecorator():
     
         
 class WireframeViewer(Widget,wf.WireframeGroup):
-    def __init__(self,rect):
+    def __init__(self,rect,data_reader):
         Widget.__init__(self,rect)
         self.wireframes = {}
         self.object_to_update = []
@@ -230,6 +247,7 @@ class WireframeViewer(Widget,wf.WireframeGroup):
                 n = 0
                 for node in nodes:
                     
+                    #Hack so that the last node is red
                     if n == nodes.shape[0]-1:
                         color = (255,0,0)
                     else:
@@ -251,34 +269,50 @@ class DataReader(threading.Thread):
     #Thread event, stops the thread if it is set.
     stopthread = threading.Event()
     
-    def __init__(self, wireframe):
+    def __init__(self):
         threading.Thread.__init__(self)                     #Call constructor of parent
-        self.ser = Serial("/dev/ttyACM1",115200)            #Initialize serial port
-        self.data_buff_size = 1024                           #Buffer size
-        self.data = np.zeros(self.data_buff_size)              #Data buffer
+        self.ser = Serial("/dev/ttyACM0",115200)            #Initialize serial port
+        self.data_buff_size = 1024                          #Buffer size
+        
+        #Data buffers for current values
+        self.datax = np.zeros(self.data_buff_size)          
         self.datay = np.zeros(self.data_buff_size)
-        self.data2 = np.zeros(self.data_buff_size)              #Data buffer
+        
+        #Data buffers for previous turn
+        self.datax2 = np.zeros(self.data_buff_size)         
         self.datay2 = np.zeros(self.data_buff_size)
-        self.x = 0
-        self.y = 0
-        self.oldx = 0
-        self.i = 0
-        self.size = 1
-        self.s0 = [0]*5
-        self.s1 = [0]*5
-        self.s2 = [0]*5
-        self.s3 = [0]*5
-        self.wireframe = wireframe
+        
+        #Lists to store the callbacks
+        self.point_listeners = []
+        self.turn_listeners = []
+        
+        self.x = 0 #Most recent x
+        self.y = 0 #Most recent y
+        self.oldx = 0 # oldx is used to keep track of when a new turn starts
+        self.i = 0 #Current index
+        self.size = 1 #Size of previous turn
         self.start()
         
     def stop(self):
         #Stop method, sets the event to terminate the thread's main loop
         self.stopthread.set()
+        
+    #Add a function to be called each time a new data point is read.
+    #listener should have x,y,i as arguments where x,y is the new point read and i is the point index.
+    def add_point_listener(self,listener):
+        self.point_listeners.append(listener)
+        
+    #Add a function to be called each time a new turn is finished.
+    #listener should have xs, ys as arguments where xs,ys are lists of numbers
+    def add_turn_listener(self,listener):
+        self.turn_listeners.append(listener)
+        
     
-    def run(self):      #Run method, this is the code that runs while thread is alive.
+    #Run method, this is the code that runs while thread is alive.
+    def run(self):      
 
-        num_bytes = 16                                     #Number of bytes to read at once
-        val = 0                                             #Read value
+        num_bytes = 16 #Number of bytes to read at once
+        val = 0 #Read value
         
         while not self.stopthread.isSet() :
             rslt = self.ser.read(num_bytes)             #Read serial data
@@ -309,150 +343,29 @@ class DataReader(threading.Thread):
         self.ser.close()
         
     def nums_read(self):
-        lock.acquire()
         # If a turn of the waveplate is complete
         if self.oldx > self.x:
             self.size = self.i
             #print self.size
             self.i = 0
-            self.data2 = self.data[:self.size].copy() #angle
+            self.datax2 = self.datax[:self.size].copy() #angle
             self.datay2 = self.datay[:self.size].copy() #intensity
-            s0, s1, s2, s3 = stokes(np.linspace(0,2*np.pi,self.size),self.datay[:self.size])
-            # Calculate 5 point moving average
-            self.s0.pop()
-            self.s1.pop()
-            self.s2.pop()
-            self.s3.pop()
-            self.s0.insert(0,s0)
-            self.s1.insert(0,s1)
-            self.s2.insert(0,s2)
-            self.s3.insert(0,s3)
-            s0 = sum(self.s0)/len(self.s0)
-            s1 = sum(self.s1)/len(self.s1)
-            s2 = sum(self.s2)/len(self.s2)
-            s3 = sum(self.s3)/len(self.s3)
-            print([s1,s2,s3])
-            (x,y,z), r = (0.5,0.5, 0.5), 0.4
-            if s0 > 0: #If the intensity is greater than zero
-                l = np.sqrt(s1**2+s2**2+s3**2)
-                self.wireframe.addNodes([(x + r*s1/l, y + r*s3/l, z + r*s2/l )])
-            self.wireframe.discardOldNodes(60)
+            for turn_listener in self.turn_listeners:
+                #Turn finished so call all of the turn listeners
+                turn_listener(self.datax2,self.datay2)
         if self.i < self.data_buff_size:
-            self.data[self.i] = self.x/1024.0*2*np.pi
+            self.datax[self.i] = self.x/1024.0*2*np.pi
             self.datay[self.i] = self.y
+            
+            for point_listener in self.point_listeners:
+                #New point measured so call all of the point listensers
+                point_listener(self.x/1024.0*2*np.pi,self.y,self.i)
             self.oldx = self.x
         else:
             print "ERROR: buffer overrun ",self.x,self.y,self.i
-        """
-        self.data[int(self.x)]= self.x
-        self.datay[int(self.x)]= self.y
-        """
-        self.i += 1
-        #print self.x, self.y
-        lock.release()                        
-                        
-"""
-class DataReader(threading.Thread):
-    
-    #Thread event, stops the thread if it is set.
-    stopthread = threading.Event()
-    
-    def __init__(self, data,wireframe):
-        threading.Thread.__init__(self) #Call constructor of parent
-        self.wireframe = wireframe
-        self.x = data[0]
-        self.y = data[1]
-        self.i = 0
-        self.j = 0
-        self.tht = 0
-        #self.ser = Serial("/dev/ttyACM0",115200)
-        self.ths = []
-        self.vals = []
-
-    def stokes(self, th, i):
-    
-        th = np.array(th)
-        i = np.array(i)
-        signal0 = i
         
-        signal1 = i*np.sin(2*th)
-        signal2 = i*np.cos(4*th)
-        signal3 = i*np.sin(4*th)
-       
+        self.i += 1                       
 
-        a = np.trapz(signal0, th)*2/np.pi
-        b = np.trapz(signal1, th)*4/np.pi
-        c = np.trapz(signal2, th)*4/np.pi
-        d = np.trapz(signal3, th)*4/np.pi
-    
-        s0 = a-c
-        s1 = 2*c
-        s2 = 2*d
-        s3 = b
-        
-        phi = 0.5*np.arctan(s2/s1)
-        xi = 0.5*np.arctan(s3/np.sqrt(s1**2+s2**2))
-        
-        return phi, xi
-    
-    def run(self):
-        #Run method, this is the code that runs while thread is alive.
-        
-        (x,y,z), r = (0.5,0.5, 0.5), 0.4
-        while not self.stopthread.isSet():
-
-            #th = 2*self.i*np.pi/200
-            #phi, xi = (np.pi*np.cos(self.j/300.)/20.,np.pi*np.sin(self.j/300.)/20.)
-            #phi, xi = (0,-np.pi*0.25*self.j/1000.)
-            #S0,S1,S2,S3 = (1,np.cos(2*phi)*np.cos(2*xi),np.sin(2*phi)*np.cos(2*xi),np.sin(2*xi))
-            #A,B,C,D = (S0+0.5*S1, S3, S1*0.5, S2*0.5)
-            #I = 0.5*(A-B*np.sin(2*th)+C*np.cos(4*th)+D*np.sin(4*th))
-
-            th = 0
-            I = 0
-            #s =  self.ser.readline().strip()
-            s  = "1 1"
-
-            try:
-                n = np.loadtxt(StringIO(s))
-                if n.shape[0] == 2:
-                    th = n[0]
-                    I = n[1]      
-                    self.ths.append(th)          
-                    self.vals.append(I)
-            except ValueError:
-                print "Value Error"
-            except IndexError:
-                print "Index Error"
-                
-            lock.acquire()
-            self.x.append(np.mod(th, np.pi))
-            self.y.append(I)
-            
-            if len(self.x) > 100:
-                self.x.pop(0)
-                self.y.pop(0)
-                
-            lock.release()
-
-            
-            if np.mod(th, np.pi) < np.mod(self.tht, np.pi):
-                phi, xi = self.stokes(self.ths,self.vals)
-                self.wireframe.addNodes([(x + r*np.sin(2*phi)*np.sin(np.pi*0.5-2*xi), y - r*np.cos(np.pi*0.5-2*xi), z - r*np.cos(2*phi)*np.sin(np.pi*0.5-2*xi) )])
-                self.ths = []
-                self.vals = []
-
-            self.i += 1
-            self.j += 1
-            #time.sleep(0.001)
-            self.wireframe.discardOldNodes(60)
-            self.tht = th
-            
-            
-    def stop(self):
-        #Stop method, sets the event to terminate the thread's main loop
-        self.stopthread.set()
-"""
 
 class Window():
     
@@ -519,15 +432,20 @@ class Window():
 class PolarisationAnalyser():
     def __init__(self):
         w = Window(800,400)
-        data = ([1],[1])
-        self.wfv = WireframeViewer((0.4,0.1,0.5,0.8))
+        
+        self.dr = DataReader()
+        self.wfv = WireframeViewer((0.4,0.1,0.5,0.8),self.dr)
         self.wfv.addWireframe('sphere', shape.Sphere((0.5,0.5, 0.5), 0.4, resolution=24), displayNodes=False)
         
         dwf = wf.Wireframe()
-        self.wfv.addWireframe('sphere_points', dwf, displayEdges=False)
+        self.wfv.addWireframe('sphere_points', dwf, displayEdges=True)
         
+        StokesCalculator(self.dr,dwf)
+        
+        """
         owf = wf.Wireframe()
         self.wfv.addWireframe('target', owf, nodeColor=(200,0,200))
+        
         
         #TODO: add target node (need Stokes parameters of target node)
         s1 = 0.93
@@ -536,8 +454,9 @@ class PolarisationAnalyser():
         (x,y,z), r = (0.5,0.5, 0.5), 0.4
         l = np.sqrt(s1**2+s2**2+s3**2)
         owf.addNodes([(x + r*s1/l, y + r*s3/l, z + r*s2/l )])
+        """
         
-        self.dr = DataReader(dwf)
+        
         osc = Oscilloscope((0.05, 0.1, 0.3, 0.8), self.dr)
         
         w.add(osc)
